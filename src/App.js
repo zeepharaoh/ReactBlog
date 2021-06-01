f042a0ddf6c48b34e70dddc302a86aaeeaf6453f
Zeyad Gomaa <zeyad@Zeyads-Macbook-Pro.local>
2021-06-02 01:50:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
+const App = () => {
+	return (
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<div className='content'>
+					<Switch>
+						<Route exact path='/'>
+							<Home />
+						</Route>
+						<Route path='/create'>
+							<Create />
+						</Route>
+						<Route path='/blogs/:id'>
+							<BlogDetails />
+						</Route>
+						<Route path='*'>
+							<NotFound />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
+};
 
 export default App;
---
+++ src/components/Create.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Create = () => {
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+	const [author, setAuthor] = useState('Noah');
+	const history = useHistory();
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		const blog = { title, body, author };
+
+		fetch('http://localhost:5000/blogs/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(blog),
+		}).then(() => {
+			history.push('/');
+		});
+	};
+
+	return (
+		<div className='create'>
+			<h2>Add a New Blog</h2>
+			<form onSubmit={handleSubmit}>
+				<label>Blog title:</label>
+				<input type='text' required value={title} onChange={e => setTitle(e.target.value)} />
+				<label>Blog body:</label>
+				<textarea required value={body} onChange={e => setBody(e.target.value)}></textarea>
+				<label>Blog author:</label>
+				<select value={author} onChange={e => setAuthor(e.target.value)}>
+					<option value='Noah'>Noah</option>
+					<option value='Gustaf'>Gustaf</option>
+				</select>
+				<button>Add Blog</button>
+			</form>
+		</div>
+	);
+};
+
+export default Create;
